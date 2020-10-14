e3a58913b5a94f5f65048eb1e39949296c76f8d1
Ahmed Butt <TypicalNewb@outlook.com>
2020-10-14 11:18:51 -0400

    added stuff

--- OOP3200-F2020-Lesson3c/Vector3D.h
+++ OOP3200-F2020-Lesson3c/Vector3D.h
@@ -2,29 +2,93 @@
 #ifndef __VECTOR3D__
 #define __VECTOR3D__
 
-#include <iostream>
 #include <string>
+
+template <class T>
 class Vector3D
 {
 public:
-	Vector3D(float x = 0.0f, float y = 0.0f, float z = 0.0f);
+	Vector3D(T x = static_cast<T>(0.0), T y = static_cast<T>(0.0), T z = static_cast<T>(0.0));
 	~Vector3D();
 
-	//Accessors
-	float GetX() const;
-	float GetY() const;
-	float GetZ() const;
+	// accessors
+	T GetX() const;
+	T GetY() const;
+	T GetZ() const;
 
-	//Mutators
-	void SetX(const float x);
-	void SetY(const float y);
-	void SetZ(const float z);
+	// mutators
+	void setX(const T x);
+	void setY(const T y);
+	void setZ(const T z);
+	void set(const T x, const T y, const T z);
 
-	std::string ToString();
+	std::string ToString() const;
 
 private:
-	float m_x;
-	float m_y;
-	float m_z;
+	T m_x;
+	T m_y;
+	T m_z;
 };
+
+/* Implementation Section */
+
+template <class T>
+Vector3D<T>::Vector3D(const T x, const T y, const T z) : m_x(x), m_y(y), m_z(z)
+{
+}
+
+template <class T>
+Vector3D<T>::~Vector3D()
+{}
+
+template <class T>
+T Vector3D<T>::GetX() const
+{
+	return m_x;
+}
+
+template <class T>
+T Vector3D<T>::GetY() const
+{
+	return m_y;
+}
+
+template <class T>
+T Vector3D<T>::GetZ() const
+{
+	return m_z;
+}
+
+template <class T>
+void Vector3D<T>::setX(const T x)
+{
+	m_x = x;
+}
+
+template <class T>
+void Vector3D<T>::setY(const T y)
+{
+	m_y = y;
+}
+
+template <class T>
+void Vector3D<T>::setZ(const T z)
+{
+	m_z = z;
+}
+
+template <class T>
+void Vector3D<T>::set(const T x, const T y, const T z)
+{
+	m_x = x;
+	m_y = y;
+	m_z = z;
+}
+
+template <class T>
+std::string Vector3D<T>::ToString() const
+{
+	return "(" + std::to_string(m_x) + ", " + std::to_string(m_y) + ", " + std::to_string(m_z) + ")";
+}
+
 #endif
